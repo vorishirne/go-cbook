049b37d5947d32be32c4e89567763b450e93093b
vorishirne <vorishirne@gmail.com>
2021-09-30 22:39:02 +0530

    creating directory with nested index
    (cherry picked from commit 4b6b08adb6fed57d0b6bd02b1a9d5f0a59f60dda)

--- pkg/convert/getlinks.go
+++ pkg/convert/getlinks.go
@@ -1,16 +1,31 @@
 package convert
 
 import (
+	"encoding/json"
+	"fmt"
 	pdf "github.com/adrg/go-wkhtmltopdf"
 	"log"
+	"net/url"
+	"os"
+	"path"
+	"strings"
 )
 
+type DirVisited struct {
+	DirPath           string
+	CountReached      int8
+	LastCounterString string
+}
 type Mod struct {
-	CssFile            string
-	BasePath           string
-	BaseDirReplaceName string
-	HistPointer        uint8
-	DisableJS          bool
+	CssFile             string
+	BaseUrl             string
+	BaseDir             string
+	HistPointer         uint8
+	ObjectOptions       json.RawMessage
+	ConverterOptions    json.RawMessage
+	Padding             map[string]int8
+	dirVisited          map[string]*DirVisited
+	BaseDirCountReached int8
 }
 
 func AllPages(urlsFilePath, modFilePath string) {
@@ -19,17 +34,93 @@ func AllPages(urlsFilePath, modFilePath string) {
 	}
 	defer pdf.Destroy()
 
-	writeConverted("https://www.envoyproxy.io/docs/envoy/latest/intro/life_of_a_request",
-		"../kindle-web/envoy/css.html", "lifeofreq", false)
-	writeConverted("https://blog.envoyproxy.io/external-c-dependency-management-in-bazel-dd37477422f5",
-		"css/medium.css", "filter1", true)
-	writeConverted("https://blog.envoyproxy.io/how-to-write-envoy-filters-like-a-ninja-part-1-d166e5abec09",
-		"css/medium.css", "filter2", true)
-	writeConverted("https://blog.envoyproxy.io/taming-a-network-filter-44adcf91517",
-		"css/medium.css", "filter3", true)
-
+	//writeConverted("https://www.envoyproxy.io/docs/envoy/latest/intro/life_of_a_request",
+	//	"../kindle-web/envoy/css.html", "lifeofreq", false)
+	//writeConverted("https://blog.envoyproxy.io/how-to-write-envoy-filters-like-a-ninja-part-1-d166e5abec09",
+	//	"css/medium.css", "filter2", true)
+	//writeConverted("https://blog.envoyproxy.io/taming-a-network-filter-44adcf91517",
+	//	"css/medium.css", "filter3", true)
+	writeConverted("https://medium.com/@abhbose6/bazel-101-2b0272b15da8",
+		"css/medium.css", "bazel2", true)
+	writeConverted("https://medium.com/@alishananda/implementing-filters-in-envoy-dcd8fc2d8fda",
+		"css/medium.css", "filter4", true)
 }
 
-func ClearUntrackedHist(modFilePath string) {
+func (m *Mod) GetFilePath(webUrl string) (dirPath, fileName string, err error) {
+
+	webUrlObj, err := url.Parse(webUrl)
+	if err != nil {
+		return "", "", err
+	}
+
+	baseUrlObj, err := url.Parse(m.BaseUrl)
+	if err != nil {
+		return "", "", err
+	}
+	webUrlPath := strings.Trim(webUrlObj.Path, "/")
+	baseUrlPath := strings.Trim(baseUrlObj.Path, "/")
+	if !strings.HasPrefix(webUrlPath, baseUrlPath) {
+		return "", "", fmt.Errorf("mod base url %v not valid for %v", webUrlPath, baseUrlPath)
+	}
+
+	dirPath = strings.Trim(strings.TrimPrefix(webUrlPath, baseUrlPath), "/")
+	if dirPath == "" {
+		return "", "", fmt.Errorf("filepath is empty")
+	}
+	lastSlash := strings.LastIndexAny(dirPath, "/")
+
+	fileName = dirPath[lastSlash+1:]
+	if lastSlash == -1 {
+		lastSlash++
+	}
+	dirPath = dirPath[:lastSlash]
+	if dirPath == "" {
+		return "", "", fmt.Errorf("no directory to write file to")
+	}
+	return
+}
+
+func (m *Mod) EnsureNumberedDir(dirPath string) (parentDir, fileCounter string, err error) {
+	if info, ok := m.dirVisited[dirPath]; ok {
+		info.CountReached++
+		return info.DirPath, info.LastCounterString + "-" + fmt.Sprintf("%03d", info.CountReached), nil
+	}
+	lastSlash := strings.LastIndexAny(dirPath, "/")
+	if lastSlash == -1 {
+		m.BaseDirCountReached++
+		newFileCounter := fmt.Sprintf("%03d", m.BaseDirCountReached)
+		newDirName := path.Join(m.BaseDir, newFileCounter+"-"+dirPath)
+
+		//common code
+		err = os.MkdirAll(newDirName, 0766)
+		if err != nil {
+			return
+		}
+		m.dirVisited[dirPath] = &DirVisited{
+			newDirName, 1, newFileCounter}
+
+		return newDirName, newFileCounter + "-" + fmt.Sprintf("%03d", 1), nil
+	}
+	suffixDirPath := dirPath[lastSlash+1:]
+	prefixDirPath := dirPath[:lastSlash]
+	newDirName, newFileCounter, err := m.EnsureNumberedDir(prefixDirPath)
+	if err != nil {
+		return
+	}
+	parentDir = path.Join(newDirName, newFileCounter+"-"+suffixDirPath)
+	fileCounter = newFileCounter + "-" + fmt.Sprintf("%03d", 1)
+
+	// common code
+	err = os.Mkdir(parentDir, 0766)
+	if err != nil {
+		return
+	}
+	m.dirVisited[dirPath] = &DirVisited{
+		parentDir, 1, newFileCounter}
+
+	return
+}
+
+func createAndUpdateDirInfo(dirName, fileCounter string) {
 
 }
---
+++ pkg/convert/getlinks_test.go
@@ -0,0 +1,33 @@
+package convert
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMod_GetFilePath(t *testing.T) {
+	m := Mod{
+		CssFile: "",
+		// here it is compulsary for scheme(https/http) to be present
+		// otherwise net package wouldn't work.
+		// hence, no need for exclusive normalization for scheme
+		BaseUrl:    "https://www.medium.com/.*",
+		BaseDir:    "envoy-blogs",
+		dirVisited: map[string]*DirVisited{},
+	}
+	filePath, fileName, err := m.GetFilePath("https://medium.com/.*/galgodas/@abhbose6/bazel-101-2b0272b15da8/")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if filePath != "galgodas/@abhbose6" {
+		t.Fatalf("path %v not expected", filePath)
+	}
+	if fileName != "bazel-101-2b0272b15da8" {
+		t.Fatalf("name %v not expected", fileName)
+	}
+	dir, index, err := m.EnsureNumberedDir(filePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(dir, index)
+}
