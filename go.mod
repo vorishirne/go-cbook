9bc4594a3b12b1f7d443f5f34b55d10bf769b1b3
vorishirne <vorishirne@gmail.com>
2021-10-12 17:34:10 +0530

    experiments with merge
    (cherry picked from commit fc845299438e25b88cd3be758308d315c8bd67c0)

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/adrg/go-wkhtmltopdf v0.2.2
 	github.com/gomarkdown/markdown v0.0.0-20210918233619-6c1113f12c4a
-	github.com/watergist/file-engine main
+	github.com/pdfcpu/pdfcpu v0.3.12
+	github.com/watergist/file-engine v1.9.1
 )
--- pkg/bookify/merge.go
+++ pkg/bookify/merge.go
@@ -1,1 +1,15 @@
 package bookify
+
+import (
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/velcrine/goreader/pkg/pdfrender"
+)
+
+func CompileToBook(m *pdfrender.Mod) (err error) {
+	inFiles := []string{"blog/go/g-01rpc/g-01-01protocols/g-01-01-01gob.pdf", "blog/go/g-02go/g-02-01pkg/g-02-01-02embed-discussion.pdf"}
+	err = api.MergeAppendFile(inFiles, "out.pdf", nil)
+	if err != nil {
+		return
+	}
+	return
+}
